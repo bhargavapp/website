5d8882f5f74a39fd2722cab114ebea5eb9c0a864
Bhargav Patel <bhargav@bhargav.app>
2024-01-05 23:23:10 +0000

    Added credits/comments for server.js

--- WEBSITE_README.md
+++
@@ -1,1 +0,0 @@
-# Welcome to the source code for my website!
--- server.js
+++ server.js
@@ -1,3 +1,12 @@
+/*
+This server.js file is used as the entry point for the server due to the special handling necessary
+for creating a certified HTTPS server that the default build/index.js will not handle.
+
+The source code for this was provided by z.houbin on StackOverflow.
+
+Regular HTTP server code is commented out due to the use case.
+*/
+
 import {handler} from './build/handler.js';
 import express from 'express';
 import fs from 'fs';
@@ -28,10 +37,10 @@ httpsServer.listen(SSLPORT, IP, function () {
     console.log('HTTPS Server is running on: https://%s:%s', IP, SSLPORT);
 });
 
-// add a route that lives separately from the SvelteKit app
+//Adds a route that lives separately from the SvelteKit app
 app.get('/healthcheck', (req, res) => {
     res.end('ok');
 });
 
-// let SvelteKit handle everything else, including serving prerendered pages and static assets
+// Passes control to SvelteKit so it can handle everything else, including serving prerendered pages and static assets
 app.use(handler);
